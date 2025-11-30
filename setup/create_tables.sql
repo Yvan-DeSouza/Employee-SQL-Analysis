53e70bc1116d1cdc3b8a3d26765986621ac5f84f
Yvan De Souza <desouzayvan01@gmail.com>
2025-11-30 18:03:16 -0500

    Adding more columns and changing schema accordingly

--- setup/create_tables.sql
+++ setup/create_tables.sql
@@ -2,5 +2,9 @@ CREATE TABLE employee (
     emp_id SERIAL PRIMARY KEY,
     emp_name TEXT NOT NULL,
     dept_name TEXT,
-    salary TEXT
+    job_title TEXT,
+    salary INT,
+    bonus INT,
+    hire_date DATE,
+    performance_score INT
 );
